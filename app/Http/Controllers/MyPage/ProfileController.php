434b9d2042097a0a1a183687a3e3d82dd068a014
daisuketerashita <ekusiad.tera@icloud.com>
2021-10-30 10:59:12 +0900

    プロフィール変更の処理

--- app/Http/Controllers/MyPage/ProfileController.php
+++ app/Http/Controllers/MyPage/ProfileController.php
@@ -5,11 +5,22 @@ namespace App\Http\Controllers\MyPage;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Mypage\Profile\EditRequest;
 
 class ProfileController extends Controller
 {
-    //
+    //プロフィール編集画面の表示
     public function showProfileEditForm(){
         return view('mypage.profile_edit_form')->with('user',Auth::user());
+    }
+
+    //プロフィール編集
+    public function editProfile(EditRequest $request){
+        $user = Auth::user();
+
+        $user->name = $request->input('name');
+        $user->save();
+
+        return redirect()->back()->with('status','プロフィールを変更しました');
     }
 }
